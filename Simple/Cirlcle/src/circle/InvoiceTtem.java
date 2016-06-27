436c12b5418f2168e8e2797b83a8ade8e5b62c6c
LamLeVan <vanlam1503@gmail.com>
2016-06-27 20:13:07 +0700

    Invoice Item

--- Simple/Cirlcle/src/circle/InvoiceTtem.java
+++ Simple/Cirlcle/src/circle/InvoiceTtem.java
@@ -11,4 +11,55 @@ package circle;
  */
 public class InvoiceTtem {
     
+    private String id;
+    private String desc;
+    private int qty;
+    private double unitPrice;
+
+    public InvoiceTtem(String id, String desc, int qty, double unitPrice) {
+        this.id = id;
+        this.desc = desc;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    
+    public double getTotal() {
+        return this.unitPrice * this.qty;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceItem["
+                + "id=" + this.getId()
+                + ", desc = " + this.getDesc()
+                + ", qty = " + this.getQty()
+                + ", unitPrice = " + this.getUnitPrice()
+                + "]";//To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
 }
